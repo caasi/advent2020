125fa183642849760bc725a18c961c28de23a653
caasi Huang <caasi.igd@gmail.com>
2020-12-22 21:19:08 +0800

    feat: solve day 3 *

--- src/Day3.hs
+++ src/Day3.hs
@@ -2,8 +2,34 @@ module Day3
     ( day3a
     ) where
 
-day3a :: String -> Geo
-day3a = read
+day3a :: String -> Int
+day3a = countGeo 3 1 . read
+
+countGeo :: Int -> Int -> Geo -> Int
+countGeo dx dy = sum . map countNode . dropY dy . map head . dropX dx . map cycle . unwrap
+
+unwrap :: Geo -> [[MapNode]]
+unwrap (Geo xs) = map unwrapCol xs
+  where unwrapCol (GeoCol ys) = ys
+
+dropX :: Int -> [[a]] -> [[a]]
+dropX dx xss = go xss 0
+  where
+    go [] _ = []
+    go (ys:yss) c = drop c ys : go yss (c + dx)
+
+dropY :: Int -> [a] -> [a]
+dropY dy xss = go xss 0
+  where
+    go [] _ = []
+    go (x:xs) c =
+      if c `mod` dy /= 0
+        then go xs (c + 1)
+        else x : go xs (c + 1)
+
+countNode :: MapNode -> Int
+countNode WithTree    = 1
+countNode WithoutTree = 0
 
 data MapNode = WithTree | WithoutTree
 
